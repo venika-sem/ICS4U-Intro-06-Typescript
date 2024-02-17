ca91e7ed86474ed225e72219771f526598377161
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2024-02-17 14:36:15 -0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* By:Venika Sem
+* Version: 1.01
+*/
+
+console.log("Hello, World!")
+
+console.log("\nDone.")
